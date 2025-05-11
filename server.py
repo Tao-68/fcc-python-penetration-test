aee04c1b8f62a53783fc991922c464ac1cceb4e0
Tao-68 <tanyuntao99@gmail.com>
2025-05-12 01:10:32 +0200

    create python scripts for server and client

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname()
+port = 444
+
+serversocket.bind((host, port))
+serversocket.listen()
+
+while True:
+    clientsocket, address - serversocket.accept()
+
+    print("Received connection from %s" % str(address))
+
+    message = 'Hello from the other side!' + "\r\n"
+    clientsocket.send(message.encode('ascii'))
+
+    clientsocket.close()
