2fbf3026b63d480dab29ee7e4bd919f983fee7d7
Tao-68 <tanyuntao99@gmail.com>
2025-05-12 14:13:21 +0200

    add scanner.py and modify client.py

--- client.py
+++ client.py
@@ -3,9 +3,10 @@ import socket
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
+addr = socket.gethostbyname(host)
 port = 444
 
-clientsocket.connect((host, port))
+clientsocket.connect((addr, port))
 
 message = clientsocket.recv(1024)
 
